f3d929cfe0f9227a5321ebd91e36b39caa69fba2
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2023-12-13 11:12:43 -0500

    Proyecto

--- src/app.py
+++ src/app.py
@@ -1,3 +1,44 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:76a59643191197465f222ee89c49f4b151c63bc69516a39e08e4ccb9fc80a9ce
-size 1345
+from flask import request, Flask
+from flask_restful import Resource, Api
+import keras
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+app = Flask(__name__)
+api = Api(app)
+
+#Cargar los modelos
+modelo = keras.models.load_model('modelo_prediccion')
+
+class Neuronitas(Resource):
+    """
+    Función para la ruta /neuronitas
+    """
+    def get(self):
+        """
+        Método GET
+        """
+        return {'message': 'William Gil Clavijo 1958471 -- Samuel Ignacio Gomez 1958829 -- Jhoan Andres Diaz 1958501'}, 200
+
+        
+class predict(Resource):
+    def post(self):
+        file = request.files['imagen']
+        if file is None:
+            return {'message': 'No se ha enviado ningún archivo'}, 400
+        else:
+            img = Image.open(file.stream).resize((224, 224)) #Leer la imagen con PIL y redimensionar
+            img = np.array(img) / 255.0 #Convertir a array y normalizar
+            res = modelo.predict(img.reshape(-1,224,224,3))
+            return {'result': f'El número es: {np.argmax(res[0])}',
+                    'total': f'El total de predicciones es {res} '}, 200
+
+api.add_resource(Neuronitas, '/neuronitas')
+api.add_resource(predict, '/predict') # Ruta /predict
+
+if __name__ == '__main__':
+    app.run(debug=True) # Ejecutar en modo de desarrollo
+
+
+        
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0b25f75b3113c2f7f245b08df8e85698b83b6310136185382c1b5f8171ec23e6
-size 79
+#!/bin/bash
+
+echo "Starting the aplication"
+python -m flask run --host=0.0.0.0
+
+
+
+    
